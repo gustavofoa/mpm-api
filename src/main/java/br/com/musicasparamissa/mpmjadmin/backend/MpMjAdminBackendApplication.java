6ff964e5f2bde21610fa4ca0aa53e89ff4587836
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2017-08-20 12:54:56 -0300

    Initial config of SpringBoot

---
+++ src/main/java/br/com/musicasparamissa/mpmjadmin/backend/MpMjAdminBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.musicasparamissa.mpmjadmin.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpMjAdminBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpMjAdminBackendApplication.class, args);
+	}
+
+}
