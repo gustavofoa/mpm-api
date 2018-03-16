9d68223b668a3181c0c03352e4a30f58e045dbe5
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2018-03-16 16:33:16 -0300

    Clear cache

--- src/main/java/br/com/musicasparamissa/api/config/ResourceConfig.java
+++ src/main/java/br/com/musicasparamissa/api/config/ResourceConfig.java
@@ -1,9 +1,11 @@
 package br.com.musicasparamissa.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableResourceServer
@@ -20,6 +22,16 @@ public class ResourceConfig extends ResourceServerConfigurerAdapter {
             .permitAll()
             .anyRequest().authenticated()
         ;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+//        addConverters(restTemplate);
+
+        return restTemplate;
     }
 
 }
---
+++ src/main/java/br/com/musicasparamissa/api/service/ClearCacheService.java
@@ -0,0 +1,54 @@
+package br.com.musicasparamissa.api.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Service
+public class ClearCacheService {
+
+    @Value("${mpm_api.cache.token.key}")
+    private String cacheTokenKey;
+    @Value("${mpm_api.cache.token.value}")
+    private String cacheTokenValue;
+    @Value("${mpm_api.cache.url.all}")
+    private String clearAllCache;
+    @Value("${mpm_api.cache.url.one}")
+    private String clearOneCache;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public boolean all(){
+
+        HttpHeaders headers = getHttpHeaders();
+
+        HttpEntity<?> request = new HttpEntity<Object>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(clearAllCache, HttpMethod.DELETE, request, String.class);
+
+        return response.getStatusCode() == HttpStatus.OK;
+
+    }
+
+    public boolean one(String url){
+
+        HttpHeaders headers = getHttpHeaders();
+
+        HttpEntity<?> request = new HttpEntity<Object>("urls[1]=" + url, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(clearOneCache, HttpMethod.DELETE, request, String.class);
+        return response.getStatusCode() == HttpStatus.ACCEPTED;
+
+    }
+
+    private HttpHeaders getHttpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(cacheTokenKey, cacheTokenValue);
+        return headers;
+    }
+
+}
