59468ca408a88000eb90a4898c5497030e8d2d35
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2018-04-12 21:01:33 -0300

    Generate site with imported template.

--- src/main/java/br/com/musicasparamissa/api/cc/service/SiteGenerateService.java
+++ src/main/java/br/com/musicasparamissa/api/cc/service/SiteGenerateService.java
@@ -8,16 +8,27 @@ import com.google.common.base.Charsets;
 import com.google.common.collect.Maps;
 import com.google.common.io.Resources;
 import com.hubspot.jinjava.Jinjava;
+import com.hubspot.jinjava.interpret.JinjavaInterpreter;
+import com.hubspot.jinjava.loader.ResourceLocator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
+import java.util.Scanner;
 
 @Slf4j
 @Service("CcSiteGenerateService")
 public class SiteGenerateService {
+
+    private static final String TEMPLATE_PATH = "static.cifrascatolicas.com.br/app/templates/";
 
     /*
      * Planejamento:
@@ -80,7 +91,7 @@ public class SiteGenerateService {
         context.put("nome", artista.getNome());
         context.put("slug", artista.getSlug());
 
-        String content = renderTemplate("cifrascatolicas/templates/artista.html", context);
+        String content = renderTemplate(TEMPLATE_PATH + "artista.html", context);
 
         siteStorage.saveFile(String.format("/%s/index.html", artista.getSlug()), content);
 
@@ -89,10 +100,25 @@ public class SiteGenerateService {
     private void generateOnlyMusica(Musica musica) {
 
         Map<String, Object> context = Maps.newHashMap();
-        context.put("nome", musica.getNome());
-        context.put("slug", musica.getSlug());
 
-        String content = renderTemplate("cifrascatolicas/templates/musica.html", context);
+        context.put("STATICPATH", "https://static.cifrascatolicas.com.br");
+
+        Map<String, Object> musicaContext = Maps.newHashMap();
+        context.put("musica", musicaContext);
+
+        musicaContext.put("slut", musica.getSlug());
+        musicaContext.put("titulo", musica.getNome());
+        musicaContext.put("cifra", musica.getCifra());
+
+        Map<String, Object> artistaContext = Maps.newHashMap();
+        musicaContext.put("artista", artistaContext);
+
+        artistaContext.put("slug", musica.getArtista().getSlug());
+        artistaContext.put("nome", musica.getArtista().getNome());
+        artistaContext.put("img", musica.getArtista().getImagem());
+
+
+        String content = renderTemplate(TEMPLATE_PATH + "musica.html", context);
 
         siteStorage.saveFile(String.format("/%s/%s/index.html", musica.getArtista().getSlug(), musica.getSlug()), content);
 
@@ -100,6 +126,17 @@ public class SiteGenerateService {
 
     private String renderTemplate(String templateName, Map<String, Object> context) {
         Jinjava jinjava = new Jinjava();
+
+        jinjava.setResourceLocator(new ResourceLocator() {
+            @Override
+            public String getString(String s, Charset charset, JinjavaInterpreter jinjavaInterpreter) throws IOException {
+
+                String filePath = TEMPLATE_PATH+s;
+
+                return getFile(filePath);
+
+            }
+        });
 
         String template = null;
         try {
@@ -109,6 +146,31 @@ public class SiteGenerateService {
         }
 
         return jinjava.render(template, context);
+    }
+
+    private String getFile(String fileName) {
+
+        StringBuilder result = new StringBuilder("");
+
+        //Get file from resources folder
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(fileName).getFile());
+
+        try (Scanner scanner = new Scanner(file)) {
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                result.append(line).append("\n");
+            }
+
+            scanner.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
+
     }
 
 }
