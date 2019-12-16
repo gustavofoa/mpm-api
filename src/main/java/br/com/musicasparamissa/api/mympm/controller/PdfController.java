6454c6b995d93de11417150970a7f2d0a09deb31
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2019-12-16 19:01:23 -0300

    PDF from HTML

--- src/main/java/br/com/musicasparamissa/api/mympm/controller/PdfController.java
+++ src/main/java/br/com/musicasparamissa/api/mympm/controller/PdfController.java
@@ -1,18 +1,31 @@
 package br.com.musicasparamissa.api.mympm.controller;
 
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 @RestController("MyMpMPdfController")
 @RequestMapping("/mympm/pdf")
 public class PdfController {
 
     @GetMapping
-    public void generate(@RequestBody String html){
+    public void generate(@RequestBody String html, HttpServletResponse response) throws Exception {
 
         System.out.println(html);
+        System.out.println("Generate PDF");
+
+        PdfRendererBuilder builder = new PdfRendererBuilder();
+        builder.useFastMode();
+        builder.withHtmlContent(html, "https://musicasparamissa.com.br/");
+        builder.toStream(response.getOutputStream());
+        builder.run();
+
     }
 
 }
