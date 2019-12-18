8ed50f97c8e5aa5854086242c194312c4b2f09e1
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2019-12-18 18:30:52 -0300

    Change generate pdf method.

--- src/main/java/br/com/musicasparamissa/api/mympm/controller/PdfController.java
+++ src/main/java/br/com/musicasparamissa/api/mympm/controller/PdfController.java
@@ -1,20 +1,18 @@
 package br.com.musicasparamissa.api.mympm.controller;
 
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
 
 @RestController("MyMpMPdfController")
 @RequestMapping("/mympm/pdf")
 public class PdfController {
 
-    @GetMapping
+    @PostMapping
     public void generate(@RequestBody String html, HttpServletResponse response) throws Exception {
 
         System.out.println(html);
